0192c908e2107f2f17b9bd84b340219389b5f0bf
Cole Rossman <colerossman99@gmail.com>
2022-02-21 12:46:45 -0800

    test for renderFamily and renderBunny pass

--- test/render-utils.test.js
+++ test/render-utils.test.js
@@ -1,18 +1,32 @@
-// IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { renderFamily, renderBunny } from '../render-utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('renderFamily should render a div with class of family and p element with family name and class of family-name', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = `<div class="family"><p class="family-name">The Zhang family</p></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderFamily({ name: 'The Zhang family' });
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
 });
+
+test('renderBunny should render a p element with the bunny name and a class of bunny-name ', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = `<p class="bunny-name">Bugs Bunny</p>`;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderBunny({ name: 'Bugs Bunny' });
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
+});
+
